48b21e6f7719e535a579375ecd4d9634dbe63564
MD Hosne Jaim Joy <jaim81200@gmail.com>
2026-05-21 00:08:54 +0600

    Code.js
    Removed all unnecessary spaces.

--- code.js
+++ code.js
@@ -1,12 +1,1 @@
-javascript:(function()
-{
-document.querySelectorAll('input[type="radio"][value="5"]').forEach(r=>{
-r.checked=true;
-r.dispatchEvent(new Event('change',{bubbles:true}));
-});
-
-var t=document.querySelector('#Comment');
-if(t){t.value="Good";t.dispatchEvent(new Event('input',{bubbles:true}));}
-
-document.querySelector('form').submit();
-})();
+javascript:(function(){document.querySelectorAll('input[type="radio"][value="5"]').forEach(r=>{r.checked=true;r.dispatchEvent(new Event('change',{bubbles:true}));});var t=document.querySelector('#Comment');if(t){t.value="Good";t.dispatchEvent(new Event('input',{bubbles:true}));}document.querySelector('form').submit();})();
